add08aa465bfb20e3f297499f2406b028d7a7228
Vivek Bhardwaj <13758088+MrTwitchyMckat@users.noreply.github.com>
2021-03-22 09:40:06 -0700

    add react frontend

---
+++ views/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
